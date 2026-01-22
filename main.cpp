9f21d68e725ad808d412799842ad36eb146c316c
musarond <amoguio@gmail.com>
2026-01-22 09:46:56 -0800

    keep adjustment from copy

--- main.cpp
+++ main.cpp
@@ -21,6 +21,9 @@ int main()
  std::cout << "Hello World!\n";
 }int main()
 {
+ super cool keep the lines
+ super cool keep the lines
+ super cool keep the lines
  std::cout << "Hello World!\n";
 }
 {
