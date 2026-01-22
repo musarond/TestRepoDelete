1f4d6379837d960763f3dece47e4f234296314bd
musarond <amoguio@gmail.com>
2026-01-22 09:41:49 -0800

    removed lines

--- main.cpp
+++ main.cpp
@@ -18,10 +18,8 @@ int main()
  std::cout << "Hello World!\n";
 }int main()
 {
- std::cout << "Hello World!\n";
-}int main()
-{
- std::cout << "Hello World!\n";
+
+
 }
 {
  std::cout << "Hello World copied copied!\n";
